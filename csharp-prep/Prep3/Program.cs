876720f6e71d05c485f7cc4e68d74066de4e06c5
Geoffrey Collins <collinsgeo@byui.edu>
2023-01-09 10:18:09 -0700

    modifed: csharp-prep/Prep3/Program.cs

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+        
+        int numberGuess = -1;
+
+        while (numberGuess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            numberGuess = int.Parse(Console.ReadLine()); 
+            if (numberGuess > magicNumber)
+            {
+                Console.WriteLine("Higher ");
+            }
+            else if (numberGuess < magicNumber)
+            {
+                Console.WriteLine("Lower ");
+            }
+            else 
+            {
+                Console.WriteLine("You guessed it! ");
+            }
+        }
     }
 }
