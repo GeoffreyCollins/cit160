1383bbb89f5da42aa85b7a873f384dd251fe5675
Geoffrey Collins <collinsgeo@byui.edu>
2023-01-10 09:21:11 -0700

    modified: csharp-prep/Prep5/Program.cs

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        static void DisplayMessage()
+        {
+            Console.WriteLine("Hello world!");
+        }
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        string userResponse = Console.ReadLine();
+        int userNumber = int.Parse(userResponse);
+        return userNumber;
+    }
+
+    static void SquareNumber(string userName, int userNumber)
+    {
+        double numberSquared = (userNumber * userNumber);
+        Console.WriteLine($"{userName}, the square of your number is {numberSquared}.");
+    }
+        DisplayMessage();
+        SquareNumber(PromptUserName(), PromptUserNumber());
     }
 }
