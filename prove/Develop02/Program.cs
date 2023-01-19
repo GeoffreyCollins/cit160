acaea8ef0c373780d72f09485b94c4d7f46d0284
Geoffrey Collins <collinsgeo@byui.edu>
2023-01-19 09:14:29 -0700

    modified: prove/Develop02/Program.cs

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        
+        int userChoice = -1;
+        Console.WriteLine("Welcome to the journal program!");
+        Console.WriteLine("Please select one of the following options:");
+        Console.WriteLine("1. Write");
+        Console.WriteLine("2. Display");
+        Console.WriteLine("3. Load");
+        Console.WriteLine("4. Save");
+        Console.WriteLine("5. Quit"); 
     }
+
+    class Write
+    {
+        string[] userOptions = {"What was the most interesting thing that happened to you today?", 
+                                "What was your favorite meal from the day?",
+                                "What would you have done differently?",
+                                "What was a good thing that happened?",
+                                "Is there anything you need to do today to prepare for tomorrow?"};
+
+    }
+        
 }
