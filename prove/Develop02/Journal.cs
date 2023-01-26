9a8bab71ac3fcab0f292094cefd2ba3f87e12b5f
Geoffrey Collins <collinsgeo@byui.edu>
2023-01-25 21:47:15 -0700

    modified: prove/Develop02/Journal.cs modified: prove/Develop02/Program.cs

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -4,15 +4,15 @@ public class Journal {
     public void Write()
     {
         // The list of random prompts for the user 
-        string[] prompts = {"What would you have done differently today? " + 
-                            "What is a positive from today? " + 
-                            "How were you of service to others? " + 
-                            "Did you do anything productive? If not, what can you do now? " + 
+        string[] prompts = {"What would you have done differently today? ", 
+                            "What is a positive from today? ",
+                            "How were you of service to others? ",
+                            "Did you do anything productive? If not, what can you do now? ", 
                             "Is there anything that needs to be done today to prepare for tomorrow? "};
         Random rand = new Random();
         int index = rand.Next(prompts.Length);
         Console.WriteLine($"Date: 01/28/2023 - Prompt: {prompts[index]} ");
         string userResponse = Console.ReadLine();
-
+        
     }
 }
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -7,13 +7,14 @@ class Program
     {   
         int userChoice = -1;
         Console.WriteLine("Welcome to the Journal Program!");
-        Console.WriteLine("Please enter one of the following choices: " +
-                        "1. Write " +
-                        "2. Display " +
-                        "3. Load " +
-                        "4. Save " +
-                        "5. Quit"); 
-        Console.WriteLine("What would you like to do? ");
+        Console.WriteLine("Please enter one of the following choices: ");
+        Console.WriteLine("1. Write ");
+        Console.WriteLine("2. Display ");            
+        Console.WriteLine("3. Load ");
+        Console.WriteLine("4. Save ");
+        Console.WriteLine("5. Quit"); 
+        Console.Write("What would you like to do? ");
+        userChoice = int.Parse(Console.ReadLine());
         
         while (userChoice != 5)
         {
