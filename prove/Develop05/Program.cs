d4db10d78d3ed43b647b506f19425ba1ac37ab66
Geoffrey Collins <collinsgeo@byui.edu>
2023-03-13 18:37:05 -0600

    new file: prove/Develop05/Goal.cs modified: prove/Develop05/Program.cs new file: prove/Develop05/goals.txt

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop05 World!");
+        Console.WriteLine("Menu options:");
+        Console.WriteLine("1. Create New Goal");
+        Console.WriteLine("2. List Goals");
+        Console.WriteLine("3. Save Goals");
+        Console.WriteLine("4. Load Goals");
+        Console.WriteLine("5. Record Event");
+        Console.WriteLine("6. Quit");
     }
 }
