2e5629808deefd8b37d612eacb1faa3221a6359c
Geoffrey Collins <collinsgeo@byui.edu>
2023-03-13 20:08:39 -0600

    modified: prove/Develop05/Goal.cs modified: prove/Develop05/Program.cs

--- prove/Develop05/Goal.cs
+++ prove/Develop05/Goal.cs
@@ -2,5 +2,60 @@ using System;
 
 class Goal
 {
-    
+    public string _goalName = "";
+    public List<Goal> _goals = new List<Goal>();
+    public void AddGoal()
+    {
+        Console.WriteLine("The types of goals are: ");
+        Console.WriteLine("    1. Simple Goal");
+        Console.WriteLine("    2. Eternal Goal");
+        Console.WriteLine("    3. Checklist Goal");
+
+        Console.WriteLine("Which type of goal would you like to create?");
+        string typeGoal = Console.ReadLine();
+
+        Console.WriteLine("What is the name of your goal?");
+        string nameOfGoal = Console.ReadLine();
+
+        Console.WriteLine("What is a short description of your goal?");
+        string descriptionGoal = Console.ReadLine();
+
+        Console.WriteLine("How many points will there be associated with this goal?");
+        int points = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("How many times does this goal need to be completed to get a bonus?");
+        int pointBonus = Convert.ToInt32(Console.ReadLine());
+
+        
+    }
+
+    public void DisplayList()
+    {
+        Console.WriteLine($"[] {_goalName}");
+        Console.WriteLine();
+    }
+
+    public void SaveGoal(string filename)
+    {
+        using (StreamWriter outputFile = new StreamWriter(filename))
+        {
+            foreach (Goal goal in _goals)
+            {
+                outputFile.WriteLine($"[] {goal}");
+            }
+        }
+    }
+    public void LoadGoal(string filename)
+    {
+        _goals.Clear();
+
+        string[] lines = System.IO.File.ReadAllLines(filename);
+        foreach(string line in lines) 
+        {
+            string[] parts = line.Split("|");
+            Goal goal = new Goal();
+            goal._goalName = parts[0];
+            _goals.Add(goal);
+        }
+    }
 }
--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,14 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
+        Goal goal = new Goal();
         MenuManager menu = new MenuManager();
+
+        int pointCount = 0;
 
         string filename = "";
 
         int userChoice = -1;
         while (userChoice != 6)
         {
+            Console.WriteLine($"You have {pointCount} points");
+
             menu.Display();
+            userChoice = Convert.ToInt32(Console.ReadLine());
+            switch (userChoice)
+            {
+                case 1:
+                    
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    Console.WriteLine("Please enter the filename that you would like to save to: ");
+                    filename = Console.ReadLine();
+                    goal.SaveGoal(filename);
+                    break;
+                case 4:
+                    Console.WriteLine("Please enter the filename that you would like to load:");
+                    filename = Console.ReadLine();
+                    goal.LoadGoal(filename);
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    Console.WriteLine("Thank you for using the goal program. Goodbye.");
+                    break;
+            }
         }
     }
 }
