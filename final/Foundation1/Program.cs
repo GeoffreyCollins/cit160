37abad7fc58006a08e789434b7db2702b2391960
Geoffrey Collins <collinsgeo@byui.edu>
2023-03-27 17:40:52 -0600

    modified: final/Foundation1/Program.cs

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        VideoInformation videoInfo = new VideoInformation();
+        Video video = new Video();
+        Comment comment = new Comment();
+
+        int choice = -1;
+
+        while (choice != 0)
+        {
+            choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    
+                    break;
+            }
+        }
+
     }
 }
