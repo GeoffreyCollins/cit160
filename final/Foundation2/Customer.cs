a79e7700f0fb5156f47a668990805971c4c60761
Geoffrey Collins <collinsgeo@byui.edu>
2023-03-28 16:02:55 -0600

    modified: final/Foundation2/Customer.cs modified: final/Foundation2/Order.cs

--- final/Foundation2/Customer.cs
+++ final/Foundation2/Customer.cs
@@ -3,18 +3,23 @@ using System;
 class Customer 
 {
     public string _name = "";
-
-    Address address = new Address();
     public void GetCustomerInfo()
     {   
+        Address address = new Address();
         Console.Write("What is your name?");
-        string name = Console.ReadLine();
+        string nameInput = Console.ReadLine();
         Console.Write("What is your street address?");
-        string address = Console.ReadLine();
+        string addressInput = Console.ReadLine();
         Console.Write("What city do you live in?");
-        string city = Console.ReadLine();
+        string cityInput = Console.ReadLine();
         Console.Write("What state/province do you live in?");
-        string state = Console.ReadLine();
+        string stateInput = Console.ReadLine();
         Console.Write("What country do you live in?");
+        address._streetAddress = addressInput;
+        address._city = cityInput;
+        address._state = stateInput;
+        Console.Write($"Customer Name: {nameInput}");
+        address.GetAddressInfo();
     }
+    
 }
--- final/Foundation2/Order.cs
+++ final/Foundation2/Order.cs
@@ -2,5 +2,15 @@ using System;
 
 class Order
 {
-    
+    public List<Product> _products = new List<Product>();
+    public List<Customer> _customers = new List<Customer>();
+
+    public void AddCustomer(Customer customer)
+    {
+        _customers.Add(customer);
+    }
+    public void AddProduct(Product product)
+    {
+        _products.Add(product);
+    }
 }
