260d68ac3ea783f0743f0d724d3b682b7820ce72
Geoffrey Collins <collinsgeo@byui.edu>
2023-03-28 15:28:19 -0600

    modified: final/Foundation1/Program.cs

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -11,11 +11,24 @@ class Program
 
         while (choice != 0)
         {
+            Console.WriteLine("Enter 1 if you would like to add a video, enter 0 to end the program");
             choice = Convert.ToInt32(Console.ReadLine());
             switch (choice)
             {
                 case 1:
-                    
+                    Console.Write("What is the name of the video?");
+                    string videoInput = Console.ReadLine();
+                    Console.Write("Who made the video?");
+                    string videoAuthor = Console.ReadLine();
+                    Console.Write("How long is the video (in seconds)?");
+                    string videoLength = Console.ReadLine();
+                    video._title = videoInput;
+                    video._author = videoAuthor;
+                    video._length = videoLength;
+                    video.display();
+                    break;
+                case 0:
+                    Console.WriteLine("Thank you for using the Youtube program!");
                     break;
             }
         }
