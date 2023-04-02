f929442540b67b77ee76c9c453ab9a29fb0c73fa
Geoffrey Collins <collinsgeo@byui.edu>
2023-04-02 14:49:31 -0600

    modified: final/Foundation3/Event.cs

--- final/Foundation3/Event.cs
+++ final/Foundation3/Event.cs
@@ -1,1 +1,24 @@
 using System;
+
+class Event 
+{
+    private string _title = "";
+    private string _desc = "";
+    private string _date = "";
+    private string _time = "";
+    private string _address = "";
+
+    public Event()
+    {
+
+    }
+
+    public Event(string title, string desc, string date, string time, string address)
+    {
+        _title = title;
+        _desc = desc;
+        _date = date;
+        _time = time;
+        _address = address;
+    }
+}
