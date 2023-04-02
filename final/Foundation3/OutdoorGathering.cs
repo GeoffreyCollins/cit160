2b90c33b2225eb13a484af6b8c7a2ca748dcca9d
Geoffrey Collins <collinsgeo@byui.edu>
2023-04-02 15:11:34 -0600

    modified: final/Foundation3/OutdoorGathering.cs

--- final/Foundation3/OutdoorGathering.cs
+++ final/Foundation3/OutdoorGathering.cs
@@ -1,1 +1,28 @@
 using System;
+
+class OutdoorGathering : Event
+{
+    public OutdoorGathering() : base("4th of July Potluck", "Party with a BBQ and Games", "July 04, 2023", "4:00 PM", "3100 Droste Rd, St. Charles, MO")
+    {
+
+    }
+
+    public OutdoorGathering(string title, string desc, string date, string time, string address) : 
+        base(title, desc, date, time, address)
+    {
+
+    }
+
+    public void PresentOutdoorGathering()
+    {
+        Console.Write("Is there any inclement weather in the forecast?");
+        string weatherInput = Console.ReadLine();
+        if (weatherInput == "no")
+        {
+            DisplayEvent();
+        }
+        else {
+            Console.WriteLine("You will want to find an indoor venue.");
+        }
+    }
+}
